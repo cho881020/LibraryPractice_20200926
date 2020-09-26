bb35f288d7fa6c99504095320cd30eb2b271ce9e
조경진 <kj_cho@idealidea.co.kr>
2020-09-26 14:05:13 +0900

    Glide를 이용해 웹상의 이미지 불러오기  => Manifest에서 권한 처리도 같이 진행

--- app/src/main/java/kr/co/tjoeun/librarypractice_20200926/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/librarypractice_20200926/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -20,6 +21,12 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        loadWebImageButton.setOnClickListener {
+
+            Glide.with(mContext).load("https://lh3.googleusercontent.com/proxy/LTmNdp347w0ZC0EqVyQjmIkuBD3ZDzfpV7qD0I2T53yfjm9gCF2iNkKfnn8gpTohMaO21CLVvQddvEy1deJ97HIvza_B7n01Ps9h7vLF29X1ZLszrlS8hTGW2fXnJTGx1YzXNMalvEiE458").into(otherPersonImg)
+
+        }
 
         profilePhotoImg.setOnClickListener {
 //            Toast.makeText(mContext, "사진 클릭됨", Toast.LENGTH_SHORT).show()
